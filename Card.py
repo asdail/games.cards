398a14b4f7dba1a7c2d5f58d3ca960043af461a9
Amit Jonas <amitjonas1@gmail.com>
2020-11-24 16:40:43 +0200

    Created Project! -Added empty .py files for every class+main -Started working on Card class

---
+++ Card.py
@@ -0,0 +1,12 @@
+class Card:
+    def __init__(self,value=0,suit="",shape=""):
+        self.value=value
+        self.suit=suit
+        self.shape=shape
+        cards={1:"Ace",2:"Two",3:"Three",4:"Four",5:"Five",6:"Six",7:"Seven",8:"Eight",9:"Nine",10:"Ten",11:"Jack",12:"Queen",13:"King"}
+
+    def value(self,value):
+        self.value=value
+        self.cards={1:"Ace",2:"Two",3:"Three",4:"Four",5:"Five",6:"Six",7:"Seven",8:"Eight",9:"Nine",10:"Ten",11:"Jack",12:"Queen",13:"King"}
+        for i in self.cards:
+            pass
