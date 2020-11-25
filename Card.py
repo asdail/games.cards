173374827fa6efa48d8c6bbdcb0ba9fc8d95a6a3
Amit Jonas <amitjonas1@gmail.com>
2020-11-25 09:50:31 +0200

    -Fixed cardvalue function -Added shapevalue function -Added __eq__ as preperation for cardIsBigger function

--- Card.py
+++ Card.py
@@ -3,8 +3,32 @@ class Card:
         self.value=value
         self.suit=suit
         self.shape=shape
+        self.cards={1:"Ace",2:"Two",3:"Three",4:"Four",5:"Five",6:"Six",7:"Seven",8:"Eight",9:"Nine",10:"Ten",11:"Jack",12:"Queen",13:"King"}
+        self.shapes={"Diamond":1,"Spade":2,"Heart":3,"Club":4}
 
     def cardvalue(self,value):
         self.value=value
-        self.cards={1:"Ace",2:"Two",3:"Three",4:"Four",5:"Five",6:"Six",7:"Seven",8:"Eight",9:"Nine",10:"Ten",11:"Jack",12:"Queen",13:"King"}
+        self.suit=self.cards[self.value]
         return self.cards[self.value]
+
+    def shapevalue(self,shape):
+        self.shape=shape
+        return self.shapes[self.shape]
+
+    def __eq__(self,other):
+        if self.value==other.value:
+            return True
+        else:
+            return False
+
+
+
+
+
+
+card=Card()
+print(card.cardvalue(13))
+print(card.value)
+print(card.suit)
+print(card.shapevalue("Diamond"))
+print(card.shape)
